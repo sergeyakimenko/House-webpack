dea0998b0e9314e81c090b9c069840c1edc6265e
sergey akimenko <saguarointexas@mail.ru>
2025-02-04 16:15:25 +0300

    without js

--- src/modules/btn.js
+++ src/modules/btn.js
@@ -2,13 +2,3 @@
 // const btnHiddenContent = document.querySelector('.btn-show-content');
 // const content = document.querySelector('.faq-main-list__hidden-content');
 
-// btnHiddenContent.addEventListener('click', function(event) {
-//     showContent(event);
-//     console.log('ck');
-// })
-
-// function showContent(event) {
-//     if (event.target === btnHiddenContent) {
-//         content.display === 'block'
-//     }
-// }
