60225cdb6e20e4fa4b64cce7f21ce3636dd09e8d
sergey akimenko <saguarointexas@mail.ru>
2025-03-01 15:44:03 +0300

    add drop-down list  in faq page

--- src/modules/btn.js
+++ src/modules/btn.js
@@ -1,4 +1,51 @@
 
 // const btnHiddenContent = document.querySelector('.btn-show-content');
-// const content = document.querySelector('.faq-main-list__hidden-content');
+const contentPrice = document.querySelectorAll('.faq-main-list__hidden-content_prices');
+const btnPrices = document.querySelector('.btn-show-content_prices');
+const btnRotatePrices = document.querySelector('.btn-rotate_prices')
+btnPrices.addEventListener('click', function () {
+    toggleClass(contentPrice, btnRotatePrices)
+})
 
+const contentTheWalk = document.querySelectorAll('.faq-main-list__hidden-content_the-walk');
+const btnTheWalk = document.querySelector('.btn-show-content_the-walk');
+const btnRotateTheWalk = document.querySelector('.btn-rotate_the-walk')
+btnTheWalk.addEventListener('click', function () {
+    toggleClass(contentTheWalk, btnRotateTheWalk)
+})
+
+const contentCapacity = document.querySelectorAll('.faq-main-list__hidden-content_capacity');
+const btnCapacity = document.querySelector('.btn-show-content_capacity');
+const btnRotateCapacity = document.querySelector('.btn-rotate_capacity')
+btnCapacity.addEventListener('click', function () {
+    toggleClass(contentCapacity, btnCapacity)
+})
+
+const contentPets = document.querySelectorAll('.faq-main-list__hidden-content_pets');
+const btnPets = document.querySelector('.btn-show-content_pets');
+const btnRotatePets = document.querySelector('.btn-rotate_pets')
+btnPets.addEventListener('click', function () {
+    toggleClass(contentPets, btnRotatePets)
+})
+
+const contentCheckIn = document.querySelectorAll('.faq-main-list__hidden-content_check-in');
+const btnCheckIn = document.querySelector('.btn-show-content_check-in');
+const btnRotateCheckIn = document.querySelector('.btn-rotate_check-in')
+btnCheckIn.addEventListener('click', function () {
+    toggleClass(contentCheckIn, btnRotateCheckIn)
+})
+
+
+function toggleClass (content, btn) {
+    content.forEach((list) => {
+        list.style.display = list.style.display === 'block' ? 'none' : 'block';
+        if (list.style.display === 'none') {
+            btn.style.transition = `transform 0.1s ease-in-out`;
+            btn.style.transform = `rotate(${0}deg)`;
+        }
+        if (list.style.display === 'block') {
+            btn.style.transition = `transform 0.1s ease-in-out`;
+            btn.style.transform = `rotate(${90}deg)`;
+        }
+    })   
+}
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,38 +1,39 @@
 
 
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
 
-    const previous = document.querySelector('.previous');
-    const next = document.querySelector('.next');
+//     const previous = document.querySelector('.previous');
+//     const next = document.querySelector('.next');
 
-    const slider = document.querySelector('.slider__container');
-    const item = document.querySelectorAll('.item');
+//     const slider = document.querySelector('.slider__content');
+//     const container = document.querySelector('slider__container')
+//     const item = document.querySelectorAll('.item');
 
-    let counter = 0;
+//     let counter = 0;
 
-    const stepSize = item[0].clientWidth;
+//     const stepSize = item[0].clientWidth;
 
-    slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
+//     slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
 
-    next.addEventListener('click', () => {
-        if (counter === item.length - 3) {
-            counter = -1;
-            console.log(counter);
-        }
-        console.log(counter);
-        slider.classList.add('transformAnim');
-        counter++;
-        slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
-    });
-    previous.addEventListener('click', () => {
+//     next.addEventListener('click', () => {
+//         if (counter === item.length - 3) {
+//             counter = -1;
+//             console.log(counter);
+//         }
+//         console.log(counter);
+//         slider.classList.add('transformAnim');
+//         counter++;
+//         slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
+//     });
+//     previous.addEventListener('click', () => {
         
-        if (counter <= 0 ) {
-            counter = item.length -1;
-            counter--;
-        }
-        slider.classList.add('transformAnim');
-        counter--;
-        slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
-    });
-});
+//         if (counter <= 0 ) {
+//             counter = item.length -1;
+//             counter--;
+//         }
+//         slider.classList.add('transformAnim');
+//         counter--;
+//         slider.style.transform = 'translateX(' + `${-stepSize * counter + (slider.clientWidth - item[0].clientWidth) / 2}px)`;
+//     });
+// });
 
